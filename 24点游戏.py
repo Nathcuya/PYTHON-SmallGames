81f43d59e9deecb28576be9b435c9d1c65c4b238
Nath <nathcuya@qiuhua.onmicrosoft.com>
2023-05-10 00:42:49 +0800

    munu

--- 24点游戏.py
+++ 24点游戏.py
@@ -16,7 +16,7 @@ def calculate_24(numbers):
             rest_numbers = [
                 numbers[k] for k in range(len(numbers)) if k != i and k != j
             ]
-            if (     
+            if (
                 calculate_24([a + b] + rest_numbers)
                 or calculate_24([a - b] + rest_numbers)
                 or calculate_24([b - a] + rest_numbers)
