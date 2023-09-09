7deb6123c101d5b10e34b1c3ec81dca1d9a9f109
Nath <nathcuya@qiuhua.onmicrosoft.com>
2023-09-09 10:10:51 +0800

    Create EatGoldCoins.py

---
+++ EatGoldCoins.py
@@ -0,0 +1,13 @@
+import os
+import configparser
+import sys
+import pygame
+import random
+
+'''游戏初始化'''
+
+def initGame():
+    #初始化pygame,设置展示窗口
+    pygame.init()
+    screen=pygame.display.set_mode(configparser.SCREENSIZE)
+    
